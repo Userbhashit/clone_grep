90df3fc42a6a5cadb4fbe6741b499ada096f4121
Bhashit Gautam <bhashitgtm004@gmail.com>
2025-06-13 12:18:44 +0530

    Updated main function

--- grep_rs/src/main.rs
+++ grep_rs/src/main.rs
@@ -1,15 +1,7 @@
-use std::{env, process};
-use grep_rs::{Config, run};
+use clap::Parser;
+use grep_rs;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments:\n{}", err);
-        process::exit(1);
-    });
-
-    if let Err(e) = run(config) {
-        println!("Error: {}", e);
-        process::exit(2);
-    }
+    let config = grep_rs::Config::parse();
+    let _ = grep_rs::run(config);
 }
